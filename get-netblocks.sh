981a121f8c551c06134a9bd7406a2f3d4a36cdae
Roscoe Pyell <roscoejp@gmail.com>
2020-09-10 12:10:20 -0400

    Update get-netblocks.sh
    Apply automatic changes

--- get-netblocks.sh
+++ get-netblocks.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 
-curl -s http://www.gstatic.com/ipranges/cloud.json | python -c 'import json,sys;obj=json.load(sys.stdin);list = [str(i.get("ipv4Prefix", i.get("ipv6Prefix", ""))) for i in obj["prefixes"] if i.get("ipv4Prefix", i.get("ipv6Prefix", "")) != ""];list.sort();print "\n".join(list);' | sort > cloud.txt
+curl -s https://www.gstatic.com/ipranges/cloud.json | python -c 'import json,sys;obj=json.load(sys.stdin);list = [str(i.get("ipv4Prefix", i.get("ipv6Prefix", ""))) for i in obj["prefixes"] if i.get("ipv4Prefix", i.get("ipv6Prefix", "")) != ""];list.sort();print "\n".join(list);' | sort > cloud.txt
